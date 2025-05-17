13889d2268e1da5213c597edb09f766c9c066355
BimoFr <bimofadhlurrahman@gmail.com>
2025-05-17 15:43:25 +0700

    Menambahkan class Mahasiswa dengan atribut private dan friend class Dosen

--- LatihanSoal9A.cpp
+++ LatihanSoal9A.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Mahasiswa {
+private:
+    string nama;
+    int nim;
+    float nilai;
+
+public:
+    Mahasiswa(string n, int i) : nama(n), nim(i), nilai(0) {}
+
+    void tampil() {
+        cout << "Nama Mahasiswa : " << nama << endl;
+        cout << "NIM            : " << nim << endl;
+        cout << "Nilai          : " << nilai << endl << endl;
+    }
+
+    friend class Dosen;
+};
