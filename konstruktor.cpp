85b0878d1556ec6b05edda41f5c91f03162d7e51
BimoFr <bimofadhlurrahman@gmail.com>
2025-05-17 14:52:16 +0700

    Memanggil class mahasiswa dengan bentuk konstruktor di fungsi main

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -36,3 +36,10 @@ void mahasiswa::cetak() {
     cout << endl << " Nim  = " << nim << endl;
     cout << " Nama = " << nama << endl;
 }
+
+int main() {
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("Indra");
+    mahasiswa mhs4(30, "Fauzan");
+}
